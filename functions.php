c74132fb253093d2539f5642b7d64fe84e4ada3e
unknown <marcio_valadares@live.com.pt>
2021-10-18 18:13:53 +0100

    Theme 18/10/2021

---
+++ functions.php
@@ -0,0 +1,100 @@
+<?php
+
+//Load all scripts and styles
+function load_scripts()
+{
+    //JS
+    wp_enqueue_script('bootstrap-js', 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js', array('jquery'), '5.1.1', true);
+
+    //CSS
+    wp_enqueue_style('bootstrap-css', 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css', array(), '5.1.1', 'all');
+    wp_enqueue_style('template', get_template_directory_uri() . '/css/template.css', array(), '1.0', 'all');
+}
+add_action('wp_enqueue_scripts', 'load_scripts');
+
+
+//Theme configuration function
+function wptheme_config()
+{
+    //Register menu
+    register_nav_menus(
+        array(
+            'my_main_menu' => 'Main Menu',
+            'footer_menu' => 'Footer Menu'
+        )
+    );
+
+    $args = array(
+        'height' => 225,
+        'width' => 1920
+    );
+
+    add_theme_support('custom-header', $args);
+    add_theme_support('post-thumbnails');
+    add_theme_support('post-formats', array('video', 'image'));
+}
+add_action('after_setup_theme', 'wptheme_config', 0);
+
+add_action('widgets_init', 'wptheme_sidebars');
+function wptheme_sidebars()
+{
+    register_sidebar(
+        array(
+            'name' => 'HomePage Sidebar',
+            'id' => 'sidebar-1',
+            'description' => 'Sidebar to be used on HomePage',
+            'before_widget' => '<div class="widget-wrapper">',
+            'after_widget' => '</div>',
+            'before_title' => '<h2 class="widget-title">',
+            'after_title' => '</h2>'
+        )
+    );
+
+    register_sidebar(
+        array(
+            'name' => 'Blog Sidebar',
+            'id' => 'sidebar-2',
+            'description' => 'Sidebar to be used on Blog Page',
+            'before_widget' => '<div class="widget-wrapper">',
+            'after_widget' => '</div>',
+            'before_title' => '<h2 class="widget-title">',
+            'after_title' => '</h2>'
+        )
+    );
+
+    register_sidebar(
+        array(
+            'name' => 'Services 1',
+            'id' => 'services-1',
+            'description' => 'First Service Area',
+            'before_widget' => '<div class="widget-wrapper">',
+            'after_widget' => '</div>',
+            'before_title' => '<h2 class="widget-title">',
+            'after_title' => '</h2>'
+        )
+    );
+
+    register_sidebar(
+        array(
+            'name' => 'Services 2',
+            'id' => 'services-2',
+            'description' => 'Second Service Area',
+            'before_widget' => '<div class="widget-wrapper">',
+            'after_widget' => '</div>',
+            'before_title' => '<h2 class="widget-title">',
+            'after_title' => '</h2>'
+        )
+    );
+
+    register_sidebar(
+        array(
+            'name' => 'Services 3',
+            'id' => 'services-3',
+            'description' => 'Third Service Area',
+            'before_widget' => '<div class="widget-wrapper">',
+            'after_widget' => '</div>',
+            'before_title' => '<h2 class="widget-title">',
+            'after_title' => '</h2>'
+        )
+    );
+}
--- index.php
+++ index.php
@@ -1,12 +1,56 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tema Wordpress</title>
-</head>
-<body>
-    <p>Parágrafo</p>
-</body>
-</html>
+<?php get_header(); ?>
+
+<img class="img-fluid" src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="" />
+
+
+<div class="content-area">
+    <main>
+        <section class="slide">
+            <div class="container">
+                <div class="row">
+                    Slide
+                </div>
+            </div>
+        </section>
+
+        <section class="services">
+            <div class="container">
+                <div class="row">
+                    Serviços
+                </div>
+            </div>
+        </section>
+
+
+        <section class="middle-area">
+            <div class="container">
+                <div class="row">
+                    
+                    <div class="news col-md-8">
+                        <?php
+                        if (have_posts()) :
+                            while (have_posts()) : the_post();
+                        ?>
+                                <?php get_template_part('template-parts/content', get_post_format()); ?>
+                                <hr>
+                            <?php
+                            endwhile;
+                        else :
+                            ?>
+                            <p>There's nothing yet to be displayed...</p>
+                        <?php endif; ?>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="map">
+            <div class="container">
+                <div class="row">
+                    Mapa
+                </div>
+            </div>
+        </section>
+    </main>
+</div>
+<?php get_footer(); ?>
